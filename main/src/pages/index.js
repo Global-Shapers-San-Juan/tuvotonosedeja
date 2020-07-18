3882c2d97d0a21b7d7d6bfb51ce0ba71efcfacc3
Xiomara Figueroa <figueroaxiomara7@gmail.com>
2020-07-18 11:39:33 -0400

    #2, Added wordpress lib

--- main/src/pages/index.js
+++ main/src/pages/index.js
@@ -8,9 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Tu Voto No Se Deja</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
