831a6bf968e166bcad073210c7b336c41cf56dff
Xiomara Figueroa <figueroaxiomara7@gmail.com>
2020-08-31 19:53:54 -0400

    Started html

---
+++ .cache/sync-requires.js
@@ -0,0 +1,10 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/xiomara/Downloads/tuvotonosedeja/.cache/dev-404-page.js")))
+}
+
---
+++ app/home.js
@@ -0,0 +1,39 @@
+
+var data = [
+  {
+    "candidateName": "Alexandra Lúgaro",
+    "candidateImage": "img/lugaro.jpg",
+    "candidateInfo": ""
+  },
+  {
+    "candidateName": "Juan Dalmau",
+    "candidateImage": "img/dalmau.jpg",
+    "candidateInfo": ""
+  }
+]
+
+var container = $('#candidates');
+
+// $.getJSON('candidates.json', function (data) {
+//   $.each(data, function (i, doc) {
+
+//     // Let's create the DOM
+//     var item = $('<div>').addClass('item'),
+//       title = $('<h1>'),
+//       info = $('<p>'),
+//       link = $('<a>');
+
+//     // Add content to the DOM
+//     link.attr('href', doc.candidateInfo)
+//       .text(doc.candidateName)
+//       .appendTo(title);
+
+//     info.text(doc.candidateInfo);
+
+//     // Append the infos to the item
+//     item.append(title, info);
+
+//     // Append the item to the container
+//     item.appendTo(container);
+//   });
+// });
